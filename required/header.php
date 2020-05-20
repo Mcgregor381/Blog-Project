3691ee1304e8bf5bc7c6cec2f1e59c60ebfd08a9
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-20 14:56:38 +0100

    test : echo

--- required/header.php
+++ required/header.php
@@ -1,12 +1,9 @@
 <?php
 require_once "required/header.php";
 
-
-generateHeader();
-
 function generateHeader()
 {
-
+echo "header call";
 
 return <<<htmlPage
 <!DOCTYPE html>
@@ -17,17 +14,17 @@ return <<<htmlPage
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Material Design for Bootstrap</title>
   <!-- MDB icon -->
-  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <link rel="icon" href="../img/mdb-favicon.ico" type="image/x-icon">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <!-- Google Fonts Roboto -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
   <!-- Material Design Bootstrap -->
-  <link rel="stylesheet" href="css/mdb.min.css">
+  <link rel="stylesheet" href="../css/mdb.min.css">
   <!-- Your custom styles (optional) -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 htmlPage;
