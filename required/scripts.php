6e58d71b1ca063da2abf20b101bfd845e7977668
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-20 15:25:52 +0100

    echo

--- required/scripts.php
+++ required/scripts.php
@@ -1,33 +1,23 @@
 <?php
-include "ui_elements/ui_SideNav.php";
 
-$html ="";
-$html .= generateScripts();
-echo $html;
-
-
-function generateScripts(){
-
-    return <<<htmlPage
-
-<body>
-
-  <!-- jQuery -->
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src=".js/mdb.min.js"></script>
-  <!-- Your custom scripts (optional) -->
-  <script type="text/javascript"></script>
-
-</body>
+echo'
+<html>
+    <body>
+    
+      <!-- jQuery -->
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+      <!-- Bootstrap tooltips -->
+      <script type="text/javascript" src="js/popper.min.js"></script>
+      <!-- Bootstrap core JavaScript -->
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+      <!-- MDB core JavaScript -->
+      <script type="text/javascript" src=".js/mdb.min.js"></script>
+      <!-- Your custom scripts (optional) -->
+      <script type="text/javascript"></script>
+    
+    </body>
 
 </html>
-
-htmlPage;
-}
+';
 
 
